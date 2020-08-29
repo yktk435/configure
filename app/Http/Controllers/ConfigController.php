9658c04a45daa21d72f6c25289c21137e2704bea
yktk435 <yokotsuka435@gmail.com>
2020-08-29 21:52:54 +0900

    初回

---
+++ app/Http/Controllers/ConfigController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+class ConfigController extends Controller
+{
+    function index()
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Hello World !');
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('hello world.xlsx');
+    }
+}
